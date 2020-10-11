8fb219a12aaaa93af5868e4650b2544d2e7b1102
Edson Junior <soneddjr@gmail.com>
2020-10-11 19:24:29 +0200

    create basic next.js setup

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Homepage - Client Boilerplate</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Homepage - Client Boilerplate
+        </h1>
+      </main>
+    </div>
+  )
+}
