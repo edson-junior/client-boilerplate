2661d601b7ebb7e8d7e18fda57d7c1b2aea9f52f
Edson Junior <soneddjr@gmail.com>
2020-10-26 17:55:33 +0100

    setup basic config for stylelint

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,15 +8,17 @@ const GlobalStyles = createGlobalStyle`
   }
 
   html {
-    font-size: 62.5%
+    font-size: 62.5%;
   }
 
-  html, body, #__next {
+  html,
+  body,
+  #__next {
     height: 100%;
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 `
 
